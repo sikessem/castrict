bd07a41e34199f746868542b31fc5acd9da67dac
Sigui Kessé Emmanuel <siguici@proton.me>
2024-09-27 04:10:29 +0100

    :arrow_up: Update dependencies and supported PHP version

--- rector.php
+++ rector.php
@@ -17,8 +17,8 @@ return static function (RectorConfig $rectorConfig): void {
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_80,
         LevelSetList::UP_TO_PHP_81,
         LevelSetList::UP_TO_PHP_82,
+        LevelSetList::UP_TO_PHP_83,
     ]);
 };
